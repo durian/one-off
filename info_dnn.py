a5c8c34c72448b086eaf04a1f428b78eb46a65f3
pberck <42381890+pberck@users.noreply.github.com>
2018-08-16 15:24:32 +0200

    New version for new format.

--- info_dnn.py
+++ info_dnn.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import minmax_scale
 #testset = pd.read_csv("testset_dnn.csv")
 #valset = pd.read_csv("validationset_dnn.csv")
 
+'''
 trainset = pd.read_csv("trainset.csv")
 trainset = trainset["T_CHASSIS"]
 trainset_list = list(trainset)
@@ -26,6 +27,7 @@ test_labels = pd.read_csv("int_labels_test.csv", names=["label"])
 test_labels = test_labels["label"]
 val_labels = pd.read_csv("int_labels_validate.csv", names=["label"])
 val_labels = val_labels["label"]
+'''
 
 '''
 triplets = Counter()
@@ -40,17 +42,17 @@ print( triplets )
 print( ids )
 '''
 
-train_hists = pd.read_csv("trainset_dnn.csv")
+train_hists = pd.read_csv("train_lT_CHASSIS_s10000_h10x10.csv", index_col=0)
 sp = 1
-st = 400 #400
-n  = 4
+st = 0 #400
+n  = 2 #4
 nn = n * n
 fig = plt.figure(figsize=(16, 16))
 plt.subplots_adjust( hspace=0.4 )
 for i in range(st, st+nn):
     ax = fig.add_subplot(n,n,sp)
     idx = i #ids.pop() # this plots same T_CHASSIS defined in tripliets loop above
-    ax.set_title( str(i)+"/"+trainset.loc[idx] ) #idx was i
+    ax.set_title( str(i) )
     im = train_hists.loc[idx,:] #idx was i
     #im = minmax_scale(im)
     #im = im / np.linalg.norm(im)
@@ -127,3 +129,88 @@ print( "Unique labels in test that are not in training", len(c) )
 s = sum( el in train_labels.values for el in test_labels.values )
 print( "Sum labels in test that are in training", s, "{:.2f}".format(s/len(test_labels.values)) )
 
+
+
+
+
+
+
+
+
+'''
+petber@ITE11527:~/Documents/HH/Health/tensorflow_roger_stockholm$ python3 info_dnn.py
+HEADS, TRAIN
+O6GY-168010    64
+O9E-167510     46
+O24E-174814    35
+W31QZ-73       29
+A-141364       26
+Name: T_CHASSIS, dtype: int64
+74789    64
+76482    46
+74705    35
+79158    29
+372      26
+Name: label, dtype: int64
+
+HEADS, TEST
+A-141364       6
+O9E-167510     4
+O6GY-168010    4
+O6YU-167502    3
+O9E-167516     3
+Name: T_CHASSIS, dtype: int64
+372      6
+76482    4
+74789    4
+75264    3
+75337    3
+Name: label, dtype: int64
+
+HEADS, VALIDATION
+A-960863       4
+O6YU-167289    4
+A-169797       3
+O6GY-169214    3
+A-163468       3
+Name: T_CHASSIS, dtype: int64
+75258    4
+57707    4
+74881    3
+75703    3
+76482    3
+Name: label, dtype: int64
+
+INTERSECTION TRAIN - VALIDATION
+train_labels 68419 unique 38725
+74789    64
+76482    46
+74705    35
+79158    29
+372      26
+Name: label, dtype: int64
+
+val_labels 3802 unique 3612
+75258    4
+57707    4
+74881    3
+75703    3
+76482    3
+Name: label, dtype: int64
+
+intersection 2358 0.034464110846402315
+Unique labels in validation that are not in training 1254
+Sum labels in validation that are in training 2520 0.66
+
+INTERSECTION TRAIN - TEST
+test_labels 3801 unique 3609
+372      6
+76482    4
+74789    4
+75264    3
+75337    3
+Name: label, dtype: int64
+intersection 2379 0.034771043131293936
+Unique labels in test that are not in training 1230
+Sum labels in test that are in training 2548 0.67
+'''
