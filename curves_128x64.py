995e7f292ad22f2b6334c75616ea4327e02f4cbb
pberck <42381890+pberck@users.noreply.github.com>
2018-08-18 08:52:49 +0200

    Add files via upload

---
+++ curves_128x64.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+import sys
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import minmax_scale
+import re
+
+files = [
+    "lENGINE_TYPE_h128x64_oAdam_lr0.0001_b0_run_.-tag-average_loss.csv",
+    "lENGINE_TYPE_h128x64_oAdam_lr0.0001_b0_run_eval-tag-accuracy.csv",
+    "lENGINE_TYPE_h128x64_oAdam_lr0.0001_b0_run_eval-tag-average_loss.csv",
+    
+    "lENGINE_TYPE_h128x64_oAdam_lr0.0001_do0.2_b1_run_.-tag-average_loss.csv",
+    "lENGINE_TYPE_h128x64_oAdam_lr0.0001_do0.2_b1_run_eval-tag-accuracy.csv",
+    "lENGINE_TYPE_h128x64_oAdam_lr0.0001_do0.2_b1_run_eval-tag-average_loss.csv",
+
+    "lENGINE_TYPE_h128x64_oAdam_lr0.001_b0_run_.-tag-average_loss.csv",
+    "lENGINE_TYPE_h128x64_oAdam_lr0.001_b0_run_eval-tag-accuracy.csv",
+    "lENGINE_TYPE_h128x64_oAdam_lr0.001_b0_run_eval-tag-average_loss.csv"
+]
+
+def extract(fn):
+    lb = ""
+    hu = ""
+    op = ""
+    lr = ""
+    do = ""
+    try:
+        bits = re.search('l(.*?)_', fn)
+        lb = bits.group(1) 
+    except:
+        pass
+        
+    try:
+        bits = re.search('h(.*?)_', fn)
+        hu = bits.group(1) 
+    except:
+        pass
+
+    try:
+        bits = re.search('o(.*?)_', fn)
+        op = bits.group(1) 
+    except:
+        pass
+
+    try:
+        bits = re.search('lr(.*?)_', fn)
+        lr = bits.group(1) 
+    except:
+        pass
+        
+    try:
+        bits = re.search('do(.*?)_', fn)
+        do = bits.group(1) 
+    except:
+        pass
+
+    return [lb, hu, op, lr, do]
+
+acc = pd.DataFrame( columns=["Step"] )
+for fn in files:
+    if not "eval-tag-accuracy" in fn:
+        continue
+
+    lb, hu, op, lr, do = extract( fn )
+    
+    lbl = lb+" "+hu+" "+op+" "+lr+" "+do
+    print( lbl )
+    nxt = pd.read_csv( fn )
+    nxt = nxt[ (nxt["Step"] <= 300000) ]
+    nxt = nxt.drop(columns=["Wall time"])
+    nxt = nxt.rename( index=str, columns={"Step": "Step", "Value": lbl} )
+    acc = pd.merge( left=acc, right=nxt, left_on='Step', right_on='Step', how='outer' )
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+acc.plot( x="Step", ax=ax )
+ax.set_title( "eval-tag-accuracy" )
+ax.set_ylim( (0.5,1) )
+ax.set_xlim( (0,300000+10000) )
+ax.legend(labelspacing=0.2, frameon=False, ncol=2)
+fig.tight_layout()
+fig.savefig("curves_128x64_eval_acc.png", dpi=144)
+
+# -----
+
+acc = pd.DataFrame( columns=["Step"] )
+for fn in files:
+    if not "eval-tag-average_loss" in fn:
+        continue
+
+    lb, hu, op, lr, do = extract( fn )
+        
+    lbl = lb+" "+hu+" "+op+" "+lr+" "+do
+    print( lbl )
+    nxt = pd.read_csv( fn )
+    nxt = nxt[ (nxt["Step"] <= 300000) ]
+    nxt = nxt.drop(columns=["Wall time"])
+    nxt = nxt.rename( index=str, columns={"Step": "Step", "Value": lbl} )
+    acc = pd.merge( left=acc, right=nxt, left_on='Step', right_on='Step', how='outer' )
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+acc.plot( x="Step", ax=ax )
+ax.set_title( "eval-tag-average_loss" )
+ax.set_ylim( (0,4) )
+ax.set_xlim( (0,300000+10000) )
+ax.legend(labelspacing=0.2, frameon=False, ncol=2)
+fig.tight_layout()
+fig.savefig("curves_128x64_eval_loss.png", dpi=144)
+
+# ----
+
+plt.show(block=True)
+
